1f795d9e7138501bb4ab9f36e01fb6fc3e71c9b6
Drew Hund <languageinvestigator@gmail.com>
2026-05-11 14:17:51 -0400

    add csv and parse data from request

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import pandas
+import pandas as pd
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -11,9 +11,9 @@ def home():
 
 @app.route("/api/v1/<station_id>/<date>")
 def about(station_id, date):
-    # df = pandas.read_csv("")
-    # temp = df.station
-    temp = 23
+    fname = f"data_small/TG_STAID{station_id.zfill(6)}.txt"
+    df = pd.read_csv(fname, skiprows=20, parse_dates=["    DATE"])
+    temp = df.loc[df["    DATE"] == date]['   TG'].squeeze() / 10
     return {
         "station_id": station_id,
         "date": date,
